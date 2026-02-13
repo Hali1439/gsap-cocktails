06d51ce522338ae15964ce71a2ba1531f15c0e30
MUKTI <halimamuk1307@gmail.com>
2026-02-13 16:28:30 +0300

    adding navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <div>App</div>
+        <main>
+            <Navbar/>
+        </main>
     )
 }
 export default App
